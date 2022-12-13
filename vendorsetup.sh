30f0daaf360dd7ca168cd841118b095554eefc2b
林凌儿 <32541118+linlinger@users.noreply.github.com>
2022-12-13 13:13:48 +0800

    Rework vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -17,68 +17,57 @@
 #
 # 	Please maintain this if you use this script or any part of it
 #
-FDEVICE="mido"
-# set -o xtrace
 
-fox_get_target_device() {
-local F="$BASH_ARGV"
-   [ -z "$F" ] && F="$BASH_SOURCE"
-   if [ -n "$F" ]; then
-      local D1=$(dirname "$F")
-      local D2=$(basename "$D1")
-      [ -n "$D2" ] && echo "$D2"
-   fi
-}
 
-if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
-   FOX_BUILD_DEVICE=$(fox_get_target_device)
-fi
+	export LC_ALL="C"
 
-if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
-   	export OF_DONT_PATCH_ENCRYPTED_DEVICE="1"
-   	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
-   	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES="1"
-   	export OF_USE_MAGISKBOOT="1"
-   	export OF_NO_MIUI_OTA_VENDOR_BACKUP="1"
-   	export OF_NO_TREBLE_COMPATIBILITY_CHECK="1"
-	export OF_USE_SYSTEM_FINGERPRINT="1"
-	export OF_ALLOW_DISABLE_NAVBAR=1
-   	export FOX_USE_BASH_SHELL=1
-   	export FOX_ASH_IS_BASH=1
-   	export FOX_USE_NANO_EDITOR=1
+	echo -e "\x1b[96mmido: Adding OrangeFox Vars...\x1b[m"
+	## 构建信息
+	# 设置显示在关于页面里的维护人员名称
+	export OF_MAINTAINER=linlinger
+	# 设置版本号为日期
+	export FOX_VERSION=$(date +%y.%m.%d)
+
+	## 添加功能
+	# 使用完整版ps命令
+	export FOX_REPLACE_BUSYBOX_PS=1
+	# 使用完整版getprop命令
+	export FOX_REPLACE_TOOLBOX_GETPROP=1
+	# 支持tar命令
 	export FOX_USE_TAR_BINARY=1
+	# 支持sed命令
 	export FOX_USE_SED_BINARY=1
+	# 使用bash代替sh和ash
+	export FOX_USE_BASH_SHELL=1
+	export FOX_ASH_IS_BASH=1
+	# 使用完整版grep命令
+	export FOX_USE_GREP_BINARY=1
+	# 支持lzma, xz命令
 	export FOX_USE_XZ_UTILS=1
-   	export FOX_REPLACE_BUSYBOX_PS=1
-	export FOX_ENABLE_APP_MANAGER=1
+	# 使用橙狐编译的NANO编辑器
+	export FOX_USE_NANO_EDITOR=1
+	# 添加phhusson's lptools工具
+	export OF_ENABLE_LPTOOLS=1
 
+	## 硬件功能设定
+	# 没有绿色led
+	export OF_USE_GREEN_LED=1
 
-   	# export OF_DISABLE_DM_VERITY_FORCED_ENCRYPTION="1"; # disabling dm-verity causes stability issues with some kernel 4.9 ROMs; but is needed for MIUI
-   	export OF_FORCE_DISABLE_DM_VERITY_MIUI="1"
-	export OF_FORCE_MAGISKBOOT_BOOT_PATCH_MIUI="1"	
-
-	# OTA for custom ROMs
-        export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
-        export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
-
-        # -- add settings for R11 --
-        export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
+        # Set quick backup items
         export OF_QUICK_BACKUP_LIST="/boot;/data;/system_image;/vendor_image;"
-        export OF_USE_TWRP_SAR_DETECT=1
-        #export FOX_ADVANCED_SECURITY=1
-        # -- end R11 settings --
 
-	# run a process after formatting data to work-around MTP issues
-	export OF_FORCE_CREATE_DATA_MEDIA_ON_FORMAT=1
+	# 当安装MIUI或者在MIUI上安装橙狐zip，使用magisk处理所有boot和recovery镜像，防止橙狐被MIUI官方recovery替换
+	export OF_FORCE_MAGISKBOOT_BOOT_PATCH_MIUI=1
+	# 避免在已加密设备上应用强制加密补丁
+	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
+	# 在MIUI OTA还原期间尝试解密内部存储（而不是错误退出）
+	export OF_OTA_RES_DECRYPT=1
+	# 防止橙狐在解密后重新运行自启动进程
+	export OF_NO_RELOAD_AFTER_DECRYPTION=1
+	# 手动刷入OTA增量包时尝试进入OpenRecoveryScript模式完成更新
+	export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
 
-	# let's log what are the build VARs that we used
-	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
-  	   export | grep "FOX" >> $FOX_BUILD_LOG_FILE
-  	   export | grep "OF_" >> $FOX_BUILD_LOG_FILE
-  	   export | grep "TW_" >> $FOX_BUILD_LOG_FILE
-  	   export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
-  	   export | grep "PLATFORM_" >> $FOX_BUILD_LOG_FILE
-  	fi
+	# 修复部分设备或者rom的解密问题
+	export OF_FIX_DECRYPTION_ON_DATA_MEDIA=1
 
-fi
-#
+	echo -e "\x1b[96mmido: Done！\x1b[m"
